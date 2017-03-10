3f71df77f30fe924c2baaf986c03fb3a1c31281f
Alexander Lemeshev <lemeshau@gmail.com>
2017-03-10 17:18:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
 using namespace std;
 
@@ -8,6 +10,7 @@ public:
 	int cols;
 	int * arr;
 public:
+Array (){arr = NULL;}
 
 Array (int n){
 	cols = n;
@@ -18,16 +21,20 @@ Array (int n){
 }
 
 ~Array(){
+	if(arr != NULL)
 	delete [] arr;
-	}
+	} 
 	
 void getArr(){
+	cout << "Array is: " << endl;
     for(int i = 0; i<cols; i++)
         cout << arr[i] << ' ';
+        cout << endl;
     }
  
 void setArr(){
-    cout << "Enter " << cols << " numbers of array across the space: " << endl;
+	
+    cout << "Enter " << cols << " the size: " << endl;
         for(int i = 0; i<cols; i++)
             cin >> arr[i];
     }
@@ -35,41 +42,44 @@ void setArr(){
 };
 
 
-class Matrix:public Array {
+class Matrix: public Array {
 public:
 	int ** matr;
-	int rows;
+	int rows, cols;
 public:
 	
 Matrix (int m){
 	rows = m;
+	cols = m;
 	matr = new int* [rows];
 	for (int i=0; i<rows; i++){
 		matr[i] = new int [cols];
 		for (int j=0; j<cols; j++){
 		matr[i][j] = 0;	
-}
-}
-}
+}}}
 
 ~Matrix(){
 	delete [] matr;
 	}
 	
 void getMatr(){
+	cout << "Massive is: " << endl;
     for (int i = 0; i<rows; i++){
 		for (int j = 0; j<cols; j++){
 			cout << matr[i][j] << ' ';
+			}
 			cout << endl;
-   }}}
+  }
+  cout << endl;
+  }
  
 void setMatr(){
-    cout << "Enter " << rows << 'x' << cols << " numbers of array across the space: " << endl;
+    cout << "Enter " << rows << 'x' << cols << " numbers of array: " << endl;
         for (int i=0; i<rows; i++){
 			for (int j=0; j<cols; j++){
             cin >> matr[i][j];
  
-			}}}
+}}}
 };
 	
 int main()
@@ -81,8 +91,8 @@ int main()
     Matrix mas2(s);
     mas1.setArr();
     mas1.getArr();
+    
     mas2.setMatr();
     mas2.getMatr();
     return 0;
 }
-
